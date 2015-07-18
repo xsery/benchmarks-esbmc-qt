a4e5028bad1ab8aac7f3dbe233a3bf728352365c
Felipe Monteiro <rms.felipe@gmail.com>
2015-07-18 14:13:30 -0400

    adding QList to regression
    Signed-off-by: Felipe Monteiro <rms.felipe@gmail.com>

---
+++ benchmarks/QList/list_empty/main.cpp
@@ -0,0 +1,24 @@
+// QList::empty
+#include <iostream>
+#include <QList>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+  QList<int> myQList;
+  int sum (0);
+
+  for (int i=1;i<=10;i++) myQList.push_back(i);
+
+  assert(!myQList.empty());
+  while (!myQList.empty())
+  {
+     sum += myQList.front();
+     myQList.pop_front();
+  }
+  assert(myQList.empty()&&(myQList.size() == 0));
+  cout << "total: " << sum << endl;
+  
+  return 0;
+}
