e8f9adb5f21b2275b084d298302f2cee26244dd6
Felipe Monteiro <rms.felipe@gmail.com>
2015-07-18 14:14:55 -0400

    adding a temporary directory to regression
    Signed-off-by: Felipe Monteiro <rms.felipe@gmail.com>

---
+++ benchmarks/tmp/QQueue/queue_constructor/main.cpp
@@ -0,0 +1,17 @@
+// constructing QQueues
+#include <iostream>
+#include <QQueue>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+    QQueue<int> QQueue;
+    QQueue.enQQueue(1);
+    QQueue.enQQueue(2);
+    QQueue.enQQueue(3);
+    while (!QQueue.isEmpty())
+        cout << QQueue.deQQueue() << endl;
+    assert(QQueue.isEmpty());
+  return 0;
+}
---
+++ benchmarks/tmp/QQueue/queue_head/main.cpp
@@ -0,0 +1,19 @@
+// QQueue::front
+#include <iostream>
+#include <QQueue>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+  QQueue<int> myQQueue;
+
+  myQQueue.push(77);
+  myQQueue.push(16);
+  assert(myQQueue.front() == 77);
+  myQQueue.front() -= myQQueue.back();    // 77-16=61
+  assert(myQQueue.front() == 61);
+  cout << "myQQueue.front() is now " << myQQueue.front() << endl;
+
+  return 0;
+}
