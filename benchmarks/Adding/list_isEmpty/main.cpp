465e97e3119d9c7a15bea7ef2faa37c9bd0f60d8
Felipe Monteiro <felipe.monteiro.mr@ieee.org>
2015-09-01 21:14:51 -0400

    adding new test cases
    Signed-off-by: Felipe Monteiro <felipe.monteiro.mr@ieee.org>

---
+++ benchmarks/Adding/list_isEmpty/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <QList>
+#include <QString>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+    QList<QString> first;
+    assert(first.isEmpty());
+  return 0;
+}
