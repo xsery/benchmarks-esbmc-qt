89475655eefceaccb1b929ce6c0969162b372044
Mário Angel <marioangelpg@gmail.com>
2015-09-04 20:23:39 -0400

    Add QtDefines and initialized implementation QThread

---
+++ libraries/Qt/QtDefines.h
@@ -0,0 +1,20 @@
+#ifndef __QTDEFINES_H__
+#define __QTDEFINES_H__
+
+#include"../../definitions.h"
+//#include<stdio.h>
+//#include<stdlib.h>
+
+#undef LONG_MAX
+#define LONG_MAX __LONG_MAX__
+
+#undef ULONG_MAX
+#define ULONG_MAX (LONG_MAX * 2UL + 1UL)
+
+
+# ifndef Q_DECL_OVERRIDE
+#  define Q_DECL_OVERRIDE
+# endif
+
+#endif /* _QTDEFINES_H__ */
+
