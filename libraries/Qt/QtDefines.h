ce2eaee21f4b2599a241b07af258f3aea1f74cea
Mário Angel <marioangelpg@gmail.com>
2015-09-16 19:22:25 -0400

    Implemantion for locmaps application add new files and past

--- libraries/Qt/QtDefines.h
+++ libraries/Qt/QtDefines.h
@@ -2,8 +2,6 @@
 #define __QTDEFINES_H__
 
 #include"../../definitions.h"
-//#include<stdio.h>
-//#include<stdlib.h>
 
 #undef LONG_MAX
 #define LONG_MAX __LONG_MAX__
@@ -11,9 +9,9 @@
 #undef ULONG_MAX
 #define ULONG_MAX (LONG_MAX * 2UL + 1UL)
 
-# ifndef Q_DECL_OVERRIDE
-#  define Q_DECL_OVERRIDE
-# endif
+#ifndef Q_DECL_OVERRIDE
+  #define Q_DECL_OVERRIDE
+#endif
 
 #endif /* _QTDEFINES_H__ */
 
