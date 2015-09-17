fec46f15af7ce8d0bd46d6ceb861e8166d811fea
Mário Angel <marioangelpg@gmail.com>
2015-09-17 01:32:35 -0400

    New benchmarks QHash remove others

--- benchmarks/QHash/hash_capacity/main.cpp
+++ benchmarks/QHash/hash_capacity/main.cpp
@@ -1,11 +1,24 @@
 #include <cassert>
 #include <QHash>
-#include <QString>
+#include <iostream>
+
 using namespace std;
 
 int main ()
 {
-    QHash<QString, int> hash;
-    assert(hash.size() == 0);
+    QHash<int, int> myQHash;
+    QHash<int, int> :: iterator it;
+
+    for(int i = 0; i < 9; i++)
+    {
+        myQHash[i] = i;
+    }
+
+    // show content:
+    for ( it=myQHash.begin() ; it != myQHash.end(); it++ )
+      cout << it.key() << " => " << it.value() << endl;
+
+
+    assert(myQHash.capacity() != 0);
     return 0;
 }
